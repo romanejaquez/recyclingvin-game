99eb6b331e6e33a0673fee6e7f1346261f54f499
Roman Jaquez <romanjaquez@Romans-MacBook-Pro.local>
2024-01-31 12:03:24 -0500

    achievements page

--- lib/pages/achievementspage.dart
+++ lib/pages/achievementspage.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_animate/flutter_animate.dart';
+import 'package:flutter_google_wallet/flutter_google_wallet_plugin.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:recyclingvin_web/helpers/styles.dart';
 import 'package:recyclingvin_web/providers/game_providers.dart';
 import 'package:recyclingvin_web/widgets/achievements/achievements_header.dart';
 import 'package:recyclingvin_web/widgets/achievements/badgedisplay.dart';
 import 'package:recyclingvin_web/widgets/backgrounds/splashbg.dart';
+import 'package:uuid/uuid.dart';
 
 class AchievementsPage extends ConsumerStatefulWidget {
   const AchievementsPage({super.key});
@@ -15,6 +16,19 @@ class AchievementsPage extends ConsumerStatefulWidget {
 }
 
 class _AchievementsPageState extends ConsumerState<AchievementsPage> {
+
+  final flutterGoogleWalletPlugin = FlutterGoogleWalletPlugin();
+  late Future<bool> _isWalletAvailable;
+
+  @override
+  void initState() {
+    super.initState();
+    _isWalletAvailable = Future(() async {
+      await flutterGoogleWalletPlugin.initWalletClient();
+      return flutterGoogleWalletPlugin.getWalletApiAvailabilityStatus();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
 
@@ -49,3 +63,66 @@ class _AchievementsPageState extends ConsumerState<AchievementsPage> {
     );
   }
 }
+
+final String _passId = const Uuid().v4();
+const String _passClass = 'bag-buster-badge';
+const String _issuerId = '3388000000022314022';
+const String _issuerEmail = 'romanejaquez@gmail.com';
+
+final String _examplePass = """ 
+    {
+      "iss": "$_issuerEmail",
+      "aud": "google",
+      "typ": "savetowallet",
+      "origins": [],
+      "payload": {
+        "genericObjects": [
+          {
+            "id": "$_issuerId.$_passId",
+            "classId": "$_issuerId.$_passClass",
+            "genericType": "GENERIC_TYPE_UNSPECIFIED",
+            "hexBackgroundColor": "#F6BA15",
+            "logo": {
+              "sourceUri": {
+                "uri": "https://romanejaquez.github.io/recyclingvin-assets/badges/recyclingvinlogo.png"
+              }
+            },
+            "cardTitle": {
+              "defaultValue": {
+                "language": "en",
+                "value": "Bag Buster Badge"
+              }
+            },
+            "subheader": {
+              "defaultValue": {
+                "language": "en",
+                "value": "Recycling Vin"
+              }
+            },
+            "header": {
+              "defaultValue": {
+                "language": "en",
+                "value": "Alex McJacobs"
+              }
+            },
+            "barcode": {
+              "type": "QR_CODE",
+              "value": "$_passId"
+            },
+            "heroImage": {
+              "sourceUri": {
+                "uri": "https://romanejaquez.github.io/recyclingvin-assets/badges/badgebagbuster.png"
+              }
+            },
+            "textModulesData": [
+              {
+                "header": "GAME ACHIEVEMENT",
+                "body": "200",
+                "id": "bags"
+              }
+            ]
+          }
+        ]
+      }
+    }
+""";
--- lib/pages/splashpage.dart
+++ lib/pages/splashpage.dart
@@ -1,17 +1,13 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
-import 'package:flutter_google_wallet/flutter_google_wallet_plugin.dart';
-import 'package:flutter_google_wallet/widget/add_to_google_wallet_button.dart';
 import 'package:recyclingvin_web/helpers/styles.dart';
 import 'package:recyclingvin_web/pages/achievementspage.dart';
 import 'package:recyclingvin_web/pages/gamepage.dart';
 import 'package:recyclingvin_web/widgets/backgrounds/splashbg.dart';
 import 'package:recyclingvin_web/widgets/achievements/achievements_btn.dart';
 import 'package:recyclingvin_web/widgets/controls/start_btn.dart';
-import 'package:recyclingvin_web/widgets/logos/duuprgameslogo.dart';
 import 'package:recyclingvin_web/widgets/logos/splashlogo.dart';
-import 'package:uuid/uuid.dart';
 
 class SplashPage extends StatefulWidget {
   const SplashPage({super.key});
@@ -22,20 +18,11 @@ class SplashPage extends StatefulWidget {
 
 class _SplashPageState extends State<SplashPage> {
   
-  final flutterGoogleWalletPlugin = FlutterGoogleWalletPlugin();
   Timer splashTimer = Timer(Duration.zero, () {});
-  late Future<bool> _isWalletAvailable;
-
 
   @override
   void initState() {
     super.initState();
-
-    super.initState();
-    _isWalletAvailable = Future(() async {
-      await flutterGoogleWalletPlugin.initWalletClient();
-      return flutterGoogleWalletPlugin.getWalletApiAvailabilityStatus();
-    });
   }
 
   @override
@@ -82,72 +69,8 @@ class _SplashPageState extends State<SplashPage> {
               ],
             ),
           ),
-          
         ],
       )
     );
   }
 }
-
-final String _passId = const Uuid().v4();
-const String _passClass = 'bag-buster-badge';
-const String _issuerId = '3388000000022314022';
-const String _issuerEmail = 'romanejaquez@gmail.com';
-
-final String _examplePass = """ 
-    {
-      "iss": "$_issuerEmail",
-      "aud": "google",
-      "typ": "savetowallet",
-      "origins": [],
-      "payload": {
-        "genericObjects": [
-          {
-            "id": "$_issuerId.$_passId",
-            "classId": "$_issuerId.$_passClass",
-            "genericType": "GENERIC_TYPE_UNSPECIFIED",
-            "hexBackgroundColor": "#F6BA15",
-            "logo": {
-              "sourceUri": {
-                "uri": "https://romanejaquez.github.io/recyclingvin-assets/badges/recyclingvinlogo.png"
-              }
-            },
-            "cardTitle": {
-              "defaultValue": {
-                "language": "en",
-                "value": "Bag Buster Badge"
-              }
-            },
-            "subheader": {
-              "defaultValue": {
-                "language": "en",
-                "value": "Recycling Vin"
-              }
-            },
-            "header": {
-              "defaultValue": {
-                "language": "en",
-                "value": "Alex McJacobs"
-              }
-            },
-            "barcode": {
-              "type": "QR_CODE",
-              "value": "$_passId"
-            },
-            "heroImage": {
-              "sourceUri": {
-                "uri": "https://romanejaquez.github.io/recyclingvin-assets/badges/badgebagbuster.png"
-              }
-            },
-            "textModulesData": [
-              {
-                "header": "GAME ACHIEVEMENT",
-                "body": "200",
-                "id": "bags"
-              }
-            ]
-          }
-        ]
-      }
-    }
-""";
